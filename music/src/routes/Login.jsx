1abaeb3f12301bf7f7f044e176f0eba0048e3e38
jeevant010 <jeevantmudgil10@gmail.com>
2025-05-05 00:20:38 +0530

    chnges

--- music/src/routes/Login.jsx
+++ music/src/routes/Login.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 // import Icon from '../components/Icon';
-
+// i will copy login component from the old project and add it here
+// the name of the component is Login but i will change it to LoginComponent and then i will import it in the app.js file and use it in the route
+// i will also add the css file for the component in the same folder as the component
+// the login page will be copied from the old project and the name of that project is tours and travels
 const Login = () => {
     return (
     <div className='w-full h-full flex flex-col  items-center'>
