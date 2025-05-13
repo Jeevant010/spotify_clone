334e17e81f63073313dc8414ea2f76142f343ad9
jeevant010 <jeevantmudgil10@gmail.com>
2025-05-13 16:08:20 +0530

    vchange

--- music/src/routes/Login.jsx
+++ music/src/routes/Login.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 // import Icon from '../components/Icon';
-// i will copy login component from the old project and add it here
-// the name of the component is Login but i will change it to LoginComponent and then i will import it in the app.js file and use it in the route
-// i will also add the css file for the component in the same folder as the component
 
 
 const Login = () => {
