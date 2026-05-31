d00719b5209b2a3c921a7b115a4481b2ca912988
jeevant010 <jeevantmudgil10@gmail.com>
2026-06-01 04:00:46 +0530

    fix: add labelClassName prop and improve input styling in PasswordInput component

--- music/src/components/shared/PasswordInput.js
+++ music/src/components/shared/PasswordInput.js
@@ -2,15 +2,15 @@
 import React from "react";
 
 
-const PasswordInput = ({label, placeholder, className, value, setValue}) =>{
+const PasswordInput = ({label, placeholder, className, value, setValue, labelClassName}) =>{
     return (
         <div className={`textInputDiv flex flex-col space-y-2 w-full ${className}`}>
-            <label htmlFor={label} className="font-semibold">
+            <label htmlFor={label} className={`font-semibold ${labelClassName}`}>
                 {label}
             </label>
             <input type="password"
             placeholder={placeholder}
-            className="p-3 border border-gray-400 border-solid rounded placeholder-grey-500"
+            className="p-3 border border-gray-500 border-solid rounded placeholder-gray-500 bg-transparent text-white focus:outline-none focus:border-white"
             id ={label}
             value={value}
             onChange={(e)=>{setValue(e.target.value)}}
