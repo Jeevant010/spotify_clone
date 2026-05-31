6ed7c8fbfff0849e9a75bbd17f8427dc321839de
jeevant010 <jeevantmudgil10@gmail.com>
2026-06-01 04:01:07 +0530

    fix: add onClick handler to TextWithHover component for improved interactivity

--- music/src/components/shared/TextWithHover.jsx
+++ music/src/components/shared/TextWithHover.jsx
@@ -1,17 +1,16 @@
 
-const TextWithHover = ({ displayText, active}) => {
+const TextWithHover = ({ displayText, active, onClick }) => {
     return(
-        <>
-            <div className="flex items-center justify-start cursor-pointer">
-                
-
-                <div className={`${
-                    active?'text-white':'text-gray-500'
-                    } font-semibold hover:text-white text-lg`} >
-                    {displayText}
-                </div>
+        <div
+            className="flex items-center justify-start cursor-pointer"
+            onClick={onClick}
+        >
+            <div className={`${
+                active ? 'text-white' : 'text-gray-500'
+                } font-semibold hover:text-white text-lg`}>
+                {displayText}
             </div>
-        </>
+        </div>
     )
 }
 
