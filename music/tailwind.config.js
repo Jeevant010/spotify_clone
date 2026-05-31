52c889d4a037a13d2d3610299b3525d93606e3b3
jeevant010 <jeevantmudgil10@gmail.com>
2026-06-01 04:00:02 +0530

    refactor: update tailwind.config.js structure and add color definitions

--- music/tailwind.config.js
+++ music/tailwind.config.js
@@ -1,22 +1,28 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {
-        fontFamily:{
-          poppins: ["Poppins", "sans-serif"],
-        },
-        height:{
-          "1/10": "10%",
-          "9/10": "90%",
-        },
-        backgroundColor:{
-          "app-black":"#121212",
-        },
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
+      height: {
+        "1/10": "10%",
+        "9/10": "90%",
+      },
+      backgroundColor: {
+        "app-black": "#121212",
+      },
+      colors: {
+        "spotify-base": "#121212",
+        "spotify-black": "#000000",
+        "spotify-card": "#181818",
+        "spotify-card-hover": "#282828",
+        "spotify-green": "#1db954",
+        "spotify-grey": "#b3b3b3",
+        "spotify-highlight": "#2a2a2a"
+      }
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
