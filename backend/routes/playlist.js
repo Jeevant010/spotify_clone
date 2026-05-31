2f00029867b8554b40b0748a28d220494a23ad94
jeevant010 <jeevantmudgil10@gmail.com>
2026-06-01 03:58:56 +0530

    feat: enhance playlist retrieval to include song artist details

--- backend/routes/playlist.js
+++ backend/routes/playlist.js
@@ -39,10 +39,11 @@ router.get(
     "/get/playlist/:playlistId",
     passport.authenticate("jwt", {session: false}),
     async (req, res) => {
-        // This concept is called req.params
         const playlistId = req.params.playlistId;
-        // I need to find a playlist with the _id = playlistId
-        const playlist = await Playlist.findOne({_id: playlistId});
+        const playlist = await Playlist.findOne({_id: playlistId}).populate({
+            path: "songs",
+            populate: {path: "artist"},
+        });
         if (!playlist) {
             return res.status(301).json({err: "Invalid ID"});
         }
